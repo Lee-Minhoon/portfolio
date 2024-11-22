6cafc7c3d7d1b72b0e30b3ade9835c34a487c308
Lee Minhoon <hackids@naver.com>
2024-11-22 17:00:17 +0900

    comment unused code

--- src/babylonjs/world-scene.ts
+++ src/babylonjs/world-scene.ts
@@ -3,31 +3,26 @@ import {
   Color3,
   Color4,
   Engine,
-  GroundMesh,
-  HemisphericLight,
-  MeshBuilder,
   Scene,
-  TransformNode,
   Vector3,
 } from "@babylonjs/core";
-import { GridMaterial } from "@babylonjs/materials";
 import { Space } from "./space";
 
 export default class WorldScene {
   private _engine: Engine;
   private _scene: Scene;
-  private _camera: ArcRotateCamera;
-  private _ground: GroundMesh;
-  private _lights: HemisphericLight[];
-  private _meshes: TransformNode[];
+  // private _camera: ArcRotateCamera;
+  // private _ground: GroundMesh;
+  // private _lights: HemisphericLight[];
+  // private _meshes: TransformNode[];
 
   constructor(canvas: HTMLCanvasElement) {
     this._engine = new Engine(canvas, true);
     this._scene = new Scene(this._engine);
     this._scene.clearColor = Color4.FromColor3(Color3.Black());
-    this._camera = this.createCamera();
+    this.createCamera();
     // this._lights = this.createLights();
-    this._meshes = this.createMeshes();
+    this.createMeshes();
     // this._ground = this.createGround();
 
     const onResize = () => {
@@ -56,22 +51,22 @@ export default class WorldScene {
     return camera;
   }
 
-  private createGround() {
-    const ground = MeshBuilder.CreateGround(
-      "ground",
-      { width: 500, height: 500 },
-      this._scene
-    );
+  // private createGround() {
+  //   const ground = MeshBuilder.CreateGround(
+  //     "ground",
+  //     { width: 500, height: 500 },
+  //     this._scene
+  //   );
 
-    const material = new GridMaterial("material", this._scene);
-    ground.material = material;
+  //   const material = new GridMaterial("material", this._scene);
+  //   ground.material = material;
 
-    return ground;
-  }
+  //   return ground;
+  // }
 
-  private createLights() {
-    return [new HemisphericLight("light", new Vector3(0, 1, 0), this._scene)];
-  }
+  // private createLights() {
+  //   return [new HemisphericLight("light", new Vector3(0, 1, 0), this._scene)];
+  // }
 
   private createMeshes() {
     return [...Space.create(this._scene)];
